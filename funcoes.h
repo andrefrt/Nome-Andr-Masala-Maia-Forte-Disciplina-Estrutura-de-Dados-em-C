c9bbf2510757f62eba08741e5995b22396b10e79
andrefrt <116937559+andrefrt@users.noreply.github.com>
2023-06-02 15:50:23 -0300

    Create funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,29 @@
+void imprimirVetor(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
+    }
+}
+
+void bubbleSort(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                int sub = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = sub;
+            }
+        }
+    }
+}
+
+void insertionSort(int vetor[], int tamanho) {
+    for (int i = 1; i < tamanho; i++) {
+        int chave = vetor[i];
+        int j = i - 1;
+        while (j >= 0 && vetor[j] > chave) {
+            vetor[j + 1] = vetor[j];
+            j = j - 1;
+        }
+        vetor[j + 1] = chave;
+    }
+}
